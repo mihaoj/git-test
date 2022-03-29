50aa1aad5a15ec315193e0645be2e9bbb148453b
mimi <mimihaojun@163.com>
2022-03-29 23:49:45 +0800

    second commit

---
+++ src/main/java/com/mi/GitTest.java
@@ -0,0 +1,11 @@
+package com.mi;
+
+/**
+ *
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git1");
+    }
+}
